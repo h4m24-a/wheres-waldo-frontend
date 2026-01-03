a210dd7b95f9394a6ea01318aa8c9acc7b7aa667
Hamza Ashraf <hamza.ashraf@live.co.uk>
2026-01-03 00:49:59 +0000

    fix typo for link component

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -7,27 +7,26 @@ import { useQuery } from "@tanstack/react-query";
 import getAllLevels from "../services/levels";
 
 export default function Homepage() {
-  const {
-    data: levels,
-    isLoading,
-    isError,
-  } = useQuery({
+  const { data: levels, isLoading, isError,} = useQuery({
     queryKey: ["levels"],
     queryFn: () => getAllLevels(),
   });
+
 
   if (isLoading) {
     return <div>Loading Levels...</div>;
   }
 
+
   if (isError || !levels) {
     return <div>Error fetching levels</div>;
   }
 
+
   return (
     <>
       <main className=" h-screen bg-black">
-        <h1 className="text-3xl border-white border-4 h-23 text-white p-3 text-center uppercase font-bungee">
+        <h1 className="text-3xl border-white border-4 text-white p-3 text-center uppercase font-bungee">
           Welcome to the Wheres Wally games!
         </h1>
         <h2 className="text-4xl  border-white border-4 text-center  rounded uppercase text-red-500  font-bungee ">
@@ -36,18 +35,19 @@ export default function Homepage() {
         </h2>{" "}
 
 
-        <div className="max-w-7xl mx-auto mt-5 ">
+        <div className="max-w-7xl mx-auto mt-5">
           <div className="grid grid-cols-3">
             {levels.map((level) => (
               <Link
-                to={`/round/start/${level.Id}`}
+                to={`/round/start/${level.id}`}
                 key={level.id}
                 className=" cursor-pointer"
               >
                 <img
-                  className=" border-white border-8 rounded-2xl  w-auto h-auto mx-auto object-contain"
+                  className=" hover:border-8 hover:border-blue-500 border-white border-8 rounded-2xl  w-auto h-auto mx-auto object-contain"
                   src={level.path}
                   alt={level.name}
+                  
                 />
                 <p className="text-center text-lg text-white font-poppins mt-1">
                   {level.name}
@@ -56,7 +56,7 @@ export default function Homepage() {
             ))}
           </div>
         </div>
-        
+
       </main>
     </>
   );
