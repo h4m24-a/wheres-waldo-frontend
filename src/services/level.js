ff74ca10ba26d47c5a7ffce7b4091bc0c7af130b
Hamza Ashraf <hamza.ashraf@live.co.uk>
2026-01-03 00:44:46 +0000

    function to fetch a single level

---
+++ src/services/level.js
@@ -0,0 +1,19 @@
+// Function call to return the selected level
+const getLevel = async (imageId) => {
+  const response = await fetch(`http://localhost:3000/round/start/${imageId}`, {
+    method: 'GET'
+  })
+
+  if (!response.ok) {
+    const data = await response.json()
+    const error = new Error(data.error || 'Error fetching level');
+    error.status = response.status;
+    throw error
+  }
+
+  const data = await response.json();   // reads response, parses it as json and returns an JS object;
+  return data;
+}
+
+
+export default getLevel;
