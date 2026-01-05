5dc81518b9467ca040510c37947ffef7123d965a
Hamza Ashraf <hamza.ashraf@live.co.uk>
2026-01-05 22:38:00 +0000

    added onClick function to retrieve name of selected character

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -1,15 +1,18 @@
-const Modal = ( { characters, top, left , className = "" } ) => {  // characters is an array of objects, top & left for position of modal using coordinates
+const Modal = ( { characters, top, left, onClick, className = "" } ) => {  // characters is an array of objects, top & left for position of modal using coordinates
   return (
     <div
-      style={{ minWidth: `${140}px`, top: `${top}px`, left: `${left}px` }}
-      className={
-        `border-2 bg-white mt-15    absolute rounded translate-x-1/5 -translate-y-1/2 ${className}`
+    style={{ minWidth: `${140}px`, top: `${top}px`, left: `${left}px` }}
+    className={
+      `border-2 bg-white mt-15    absolute rounded translate-x-1/5 -translate-y-1/2 ${className}` // className with default styles, className prop used to add additional styles
       }
     >
+    
 
       {characters.map((character) => {
         return (
-          <div key={character.name} className=" grid grid-cols-2  place-items-center px-2 rounded py-0.5  transition-transform duration-300 ease-in-out  hover:shadow overflow-hidden gap-1 cursor-pointer">
+          <div 
+            onClick={() => onClick(character.name)} // passing character name to parent component
+            key={character.name} className=" grid grid-cols-2  place-items-center px-2 rounded py-0.5  transition-transform duration-300 ease-in-out  hover:shadow overflow-hidden gap-1 cursor-pointer">
             <img
             src={character.image}
             alt={character.name}
@@ -29,3 +32,13 @@ const Modal = ( { characters, top, left , className = "" } ) => {  // characters
 
 
 export default Modal
+
+
+/*
+When a character is clicked in the modal, the onClick handler will be called with character.name as an argument. (the HandleCharacterName function from the parent) will be triggered.
+
+Modal component uses an inline arrow function to pass the specific character.name to the HandleCharacterName function in game.jsx
+
+The parent component HandleCharacterName in game.jsx will receive the character.name and then it stores in the characterName state
+
+*/
