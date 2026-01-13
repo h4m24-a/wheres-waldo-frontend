7c2530947d3c8599b16fa563489668dfba094427
Hamza Ashraf <hamza.ashraf@live.co.uk>
2026-01-13 01:13:57 +0000

    update validate guess

--- src/services/validateGuess.js
+++ src/services/validateGuess.js
@@ -1,15 +1,15 @@
 // Validating Guess - Accepts x, y & character_name
 const valiadateGuess = async (imageId, x, y, character_name) => {
-  const repsonse = await fetch('http://localhost:3000/guess', {
+  const response = await fetch('http://localhost:3000/guess', {
     method: 'POST',
     credentials: 'include',
     headers: { "Content-Type": "application/json"},
     body: JSON.stringify({ imageId, x, y, character_name })
   }) 
 
-    const data = await repsonse.json();   // reads response, parses it as json and returns an JS object;
+    const data = await response.json();   // reads response, parses it as json and returns an JS object;
 
-  if (!repsonse.ok) {
+  if (!response.ok) {
     const error = new Error(data.error || 'Error fetching current round');
     error.status = data.status;
     throw error
